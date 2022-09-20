091d647e7bd93c56c74c8b362e4af7ae9adf91a8
adam <adam@quarterspot.com>
2022-09-20 09:33:05 -0400

    initial push

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Diagnostics;
+using MethodTimer;
+using System.Reflection;
+
+await Test.TimedMethod();
+
+public class Test
+{
+    [Time]
+    public static async Task TimedMethod()
+    {
+        await Task.Delay(1000);
+    }
+}
+public static class MethodTimeLogger
+{
+    public static void Log(MethodBase methodBase, long milliseconds, string message)
+    {
+
+        Debug.WriteLine($"Time Ms: {milliseconds}");
+    }
+}
